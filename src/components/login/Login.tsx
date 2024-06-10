7951e30acfc7d9a30703e761ffb377fbc9f64bab
21130521 <21130521@st.hcmuaf.edu.vn>
2024-06-10 23:24:53 +0700

    Eighth Commit

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,13 +1,18 @@
 import {useState} from "react";
 import "./login.css"
 import {toast} from "react-toastify";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import {doc, setDoc} from "firebase/firestore";
+import upload from "../../lib/upload";
+import {auth, db} from "../../lib/firebase";
 
 const Login = () => {
-
   const [avatar, setAvatar] = useState({
     file: null,
     url: ""
   })
+
+  const [loading, setLoading] = useState(false);
 
   const handleAvatar = e => {
     if (e.target.files[0]) {
@@ -18,9 +23,53 @@ const Login = () => {
     }
   }
 
-  const handleLogin = e => {
+  const handleLogin = async (e) => {
     e.preventDefault() // prevent refreshing the page
-    toast.success("Login successfully!");
+    setLoading(true);
+    const formData = new FormData(e.target);
+    const {email, password} = Object.fromEntries(formData);
+
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password);
+
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const handleRegister = async e => {
+    e.preventDefault() // prevent refreshing the page
+    setLoading(true);
+    const formData = new FormData(e.target)
+    const {username, email, password} = Object.fromEntries(formData);
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const imgURL = await upload(avatar.file);
+
+      await setDoc(doc(db, "users", res.user.uid), {
+        username,
+        email,
+        avatar: imgURL,
+        id: res.user.uid,
+        blocked: [],
+      });
+
+      await setDoc(doc(db, "userchats", res.user.uid), {
+        chats: []
+      });
+
+      toast.success("Account successfully registered!");
+
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -30,15 +79,15 @@ const Login = () => {
           <form onSubmit={handleLogin}>
             <input type="text" placeholder="Email" name="email"/>
             <input type="password" placeholder="Password" name="password"/>
-            <button>Sign In</button>
+            <button disabled={loading}>{loading ? "Loading" : "Sign In"}</button>
           </form>
         </div>
 
         <div className="separator"></div>
 
         <div className="item">
-          <h2>Welcome back,</h2>
-          <form>
+          <h2>Create an Account</h2>
+          <form onSubmit={handleRegister}>
             <label htmlFor="file">
               <img src={avatar.url || "./avatar.png"} alt=""/>
               Upload an image</label>
@@ -46,7 +95,7 @@ const Login = () => {
             <input type="text" placeholder="Username" name="username"/>
             <input type="text" placeholder="Email" name="email"/>
             <input type="password" placeholder="Password" name="password"/>
-            <button>Sign Up</button>
+            <button disabled={loading}>{loading ? "Loading" : "Sign Up"}</button>
           </form>
         </div>
 
