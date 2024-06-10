f91a3ff4dbe4ddcc8f551a645280872cc49a00e0
21130521 <21130521@st.hcmuaf.edu.vn>
2024-06-10 23:17:49 +0700

    Fifth Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import Detail from "./components/detail/Detail.js";
 import Chat from "./components/chat/Chat.js";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
+import {useEffect} from "react";
 
 const App = () => {
 
   const user = true;
+
+  useEffect(()=>{
+
+  })
 
   return (
       <div className='container'>
---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
+
+const firebaseConfig = {
+  // apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: "AIzaSyAUO_8BTdLyRx7OV3DeLieNjb3riNlwJUM",
+  authDomain: "react-firebase-chatapp-86f18.firebaseapp.com",
+  projectId: "react-firebase-chatapp-86f18",
+  storageBucket: "react-firebase-chatapp-86f18.appspot.com",
+  messagingSenderId: "223202577414",
+  appId: "1:223202577414:web:078752ba78dc0d1cb78787",
+  measurementId: "G-EN7TYEHV5B"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
